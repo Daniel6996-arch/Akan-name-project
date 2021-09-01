ec1b7ca872b291c834dbeb11d833f8b1773394ee
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-01 15:53:11 +0300

    add control_flows

--- main.js
+++ main.js
@@ -1,35 +1,20 @@
-//declare variables
-var year = document.getElementById('year').value;
-var month = document.getElementById('month').value;
-var day = document.getElementById('day').value; 
+
+
 var btn = document.getElementById('btn');
-
-//logic
-/** var year = 1978;
-var yr = year.toString()
-var y = yr.slice(0,2)
-var CC = parseInt(y);
-console.log(CC); */
-
-//deriving value of MM
-var MM = parseInt(month);
-//deriving value of DD
-var DD = parseInt(day);
 
 btn.addEventListener('click', function(){
 years()
 });
 
 function years(){
-    var year = document.getElementById('year').value;
-    var yr = year.toString();
-    var y = yr.slice(0,2);
-    var CC = parseInt(y);
-
-    var yr = year.toString();
-    var y = yr.slice(2,4);
-    var YY = parseInt(y);
-
-    
-
+    var CC = document.getElementById('year').value.slice(0,2);
+    var YY = document.getElementById('year').value.slice(2,4);
+    var MM = document.getElementById('month').value;
+    var DD = document.getElementById('day').value; 
+    if(DD>31 || DD<=0 ){
+        alert("Invalid date")
+    }
+    if(MM<=0 || MM>12){
+        alert("Invalid month")
+    }
 }
