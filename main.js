968b9663d7582312f244af8836c514c3a782478a
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-01 22:27:55 +0300

    edit styling of article

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function years(){
     if(MM<=0 || MM>12){
         alert("Invalid month")
     }
-    var dayOfWeek = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)
+    var dayOfWeek = Math.floor((((CC/4)-2*CC-1)+(5*YY/4)+(26*(MM+1)/10)+DD)%7);
     console.log(dayOfWeek);
 }
  
