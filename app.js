6e4d9698a6865e76a3a1b6f60c32a2e4c12c6550
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-03 14:17:38 +0300

    add initial operational js code

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
-var maleName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 
-var femaleName = ['Akosua', 'Adwoa', 'Abenaa','Akua', 'Yaa', 'Afua', 'Ama'];
+var select = document.getElementById('gender');
+
+
 
 var btn = document.getElementById('btn');
 
 
 
 btn.addEventListener('click', function(){
-
-var CC = document.getElementById('year').value.slice(0,2);
-var YY = document.getElementById('year').value.slice(2,4);
-var MM = document.getElementById('month').value;
-var DD = document.getElementById('day').value; 
-var dayOfWeek = Math.floor((((CC/4)-2*CC-1)+(5*YY/4)+(26*(MM+1)/10)+DD)%7);
-
-    if(DD>31 || DD<=0 ){
-        alert("Invalid date")
-    }
-    if(MM<=0 || MM>12){
-        alert("Invalid month")
-    }
-     console.log(dayOfWeek, femaleName[dayOfWeek]);
-
+    
+    var option = select[select.selectedIndex].text;
+    //console.log(option)
+     if(option == "Male"){
+         //console.log('male')
+     }else{
+         //console.log('female')
+     }
 });
 
- 
-
-  
-
-   
-
-
- 
--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 var maleName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 
-var colors  = ['red', 'green', 'yellow'];
+var femaleName = ['Akosua', 'Adwoa', 'Abenaa','Akua', 'Yaa', 'Afua', 'Ama'];
 
-var button = document.getElementById('btn');
+var btn = document.getElementById('btn');
 
-button.addEventListener('click', function(){
-    const randomNumber = getRandomNumber();
-document.body.style.backgroundColor = colors[randomNumber];
+var select = document.getElementById('gender');
 
-console.log(maleName[randomNumber]);
+btn.addEventListener('click', function(){
+
+var CC = document.getElementById('year').value.slice(0,2);
+var YY = document.getElementById('year').value.slice(2,4);
+var MM = document.getElementById('month').value;
+var DD = document.getElementById('day').value; 
+var dayOfWeek = Math.floor((((CC/4)-2*CC-1)+(5*YY/4)+(26*(MM+1)/10)+DD)%7);
+
+    if(DD>31 || DD<=0){
+        alert("Invalid date")
+    }
+    if(MM<=0 || MM>12){
+        alert("Invalid month")
+    }
+     //console.log(dayOfWeek, femaleName[dayOfWeek]);
+     var option = select[select.selectedIndex].text;
+    //console.log(option)
+     if(option == "Male"){
+         //console.log('male')
+         console.log(maleName[dayOfWeek]);
+     }else{
+         //console.log('female')
+         console.log(femaleName[dayOfWeek]);
+     }
+
 });
 
-function getRandomNumber(){
-    return Math.floor(Math.random() * colors.length);
-}
+ 
+
+  
+
+   
+
+
+ 
